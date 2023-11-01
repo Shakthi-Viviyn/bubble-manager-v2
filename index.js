7baa3981f6ea058a92b957aa6dd9d795a6e4c15b
Shakthi-Viviyn <55011804+Shakthi-Viviyn@users.noreply.github.com>
2023-11-01 01:05:13 -0400

    rest of the text added fully

--- index.js
+++ index.js
@@ -7,37 +7,37 @@ const currentDate = new Date();
 document.getElementById("dateTask").setAttribute("min",currentDate.toISOString().slice(0,10));
 
 var nodes = [
-  { name: "aaaaaaa", id: 1, x: 100, y: 100, r: 120, color: "#DB5657", description: "This is a random description of \nlength 25", timeRequired: "12h 30m" },
-  { name: "DHFJGHD", id: 2, x: 200, y: 200, r: 50, color: "#67BB6D", description: "This is a random description of length 25", timeRequired: "8h 45m" },
-  { name: "JFGHDFG", id: 3, x: 300, y: 300, r: 40, color: "#3DC7C7", description: "This is a random description of length 25", timeRequired: "5h 15m" },
-  { name: "KJGDFHD", id: 4, x: 400, y: 400, r: 100, color: "#D4DC6C", description: "This is a random description of length 25", timeRequired: "10h 0m" },
-  { name: "DHFJGHD", id: 5, x: 500, y: 500, r: 50, color: "#9292C8", description: "This is a random description of length 25", timeRequired: "6h 30m" },
-  { name: "JFGHDFG", id: 6, x: 600, y: 600, r: 40, color: "#60DAAC", description: "This is a random description of length 25", timeRequired: "3h 45m" },
-  { name: "KJGDFHD", id: 7, x: 700, y: 700, r: 100, color: "#DB5657", description: "This is a random description of length 25", timeRequired: "9h 0m" },
-  { name: "DHFJGHD", id: 8, x: 800, y: 800, r: 50, color: "#67BB6D", description: "This is a random description of length 25", timeRequired: "4h 15m" },
-  { name: "JFGHDFG", id: 9, x: 900, y: 900, r: 40, color: "#3DC7C7", description: "This is a random description of length 25", timeRequired: "7h 30m" },
-  { name: "KJGDFHD", id: 10, x: 100, y: 900, r: 100, color: "#D4DC6C", description: "This is a random description of length 25", timeRequired: "11h 15m" },
-  { name: "DHFJGHD", id: 11, x: 200, y: 800, r: 50, color: "#9292C8", description: "This is a random description of length 25", timeRequired: "2h 45m" },
-  { name: "JFGHDFG", id: 12, x: 300, y: 700, r: 40, color: "#60DAAC", description: "This is a random description of length 25", timeRequired: "8h 0m" },
-  { name: "KJGDFHD", id: 13, x: 400, y: 600, r: 100, color: "#DB5657", description: "This is a random description of length 25", timeRequired: "5h 45m" },
-  { name: "DHFJGHD", id: 14, x: 500, y: 500, r: 50, color: "#67BB6D", description: "This is a random description of length 25", timeRequired: "12h 0m" },
-  { name: "JFGHDFG", id: 15, x: 600, y: 400, r: 40, color: "#3DC7C7", description: "This is a random description of length 25", timeRequired: "6h 15m" },
-  { name: "KJGDFHD", id: 16, x: 700, y: 300, r: 100, color: "#D4DC6C", description: "This is a random description of length 25", timeRequired: "9h 30m" },
-  { name: "DHFJGHD", id: 17, x: 800, y: 200, r: 50, color: "#9292C8", description: "This is a random description of length 25", timeRequired: "4h 45m" },
-  { name: "JFGHDFG", id: 18, x: 900, y: 100, r: 40, color: "#60DAAC", description: "This is a random description of length 25", timeRequired: "7h 0m" },
-  { name: "KJGDFHD", id: 19, x: 100, y: 100, r: 100, color: "#DB5657", description: "This is a random description of length 25", timeRequired: "10h 15m" },
-  { name: "DHFJGHD", id: 20, x: 200, y: 200, r: 50, color: "#67BB6D", description: "This is a random description of length 25", timeRequired: "3h 30m" },
-  { name: "JFGHDFG", id: 21, x: 300, y: 300, r: 40, color: "#3DC7C7", description: "This is a random description of length 25", timeRequired: "6h 45m" },
-  { name: "KJGDFHD", id: 22, x: 400, y: 400, r: 100, color: "#D4DC6C", description: "This is a random description of length 25", timeRequired: "11h 0m" },
-  { name: "DHFJGHD", id: 23, x: 500, y: 500, r: 50, color: "#9292C8", description: "This is a random description of length 25", timeRequired: "5h 30m" },
-  { name: "JFGHDFG", id: 24, x: 600, y: 600, r: 40, color: "#60DAAC", description: "This is a random description of length 25", timeRequired: "8h 45m" },
-  { name: "KJGDFHD", id: 25, x: 700, y: 700, r: 100, color: "#DB5657", description: "This is a random description of length 25", timeRequired: "4h 0m" },
-  { name: "DHFJGHD", id: 26, x: 800, y: 800, r: 50, color: "#67BB6D", description: "This is a random description of length 25", timeRequired: "9h 15m" },
-  { name: "JFGHDFG", id: 27, x: 900, y: 900, r: 40, color: "#3DC7C7", description: "This is a random description of length 25", timeRequired: "2h 30m" },
-  { name: "KJGDFHD", id: 28, x: 100, y: 900, r: 100, color: "#D4DC6C", description: "This is a random description of length 25", timeRequired: "7h 45m" },
-  { name: "DHFJGHD", id: 29, x: 200, y: 800, r: 50, color: "#9292C8", description: "This is a random description of length 25", timeRequired: "11h 0m" },
-  { name: "JFGHDFG", id: 30, x: 300, y: 700, r: 40, color: "#60DAAC", description: "This is a random description of length 25", timeRequired: "5h 15m" }
-];
+    { name: "aaaaaaa", id: 1, x: 100, y: 100, r: 100, color: "#DB5657", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "12h 30m" },
+    { name: "DHFJGHD", id: 2, x: 200, y: 200, r: 80, color: "#67BB6D", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "8h 45m" },
+    { name: "JFGHDFG", id: 3, x: 300, y: 300, r: 50, color: "#3DC7C7", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "5h 15m" },
+    { name: "KJGDFHD", id: 4, x: 400, y: 400, r: 100, color: "#D4DC6C", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "10h 0m" },
+    { name: "DHFJGHD", id: 5, x: 500, y: 500, r: 80, color: "#9292C8", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "6h 30m" },
+    { name: "JFGHDFG", id: 6, x: 600, y: 600, r: 50, color: "#60DAAC", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "3h 45m" },
+    { name: "KJGDFHD", id: 7, x: 700, y: 700, r: 100, color: "#DB5657", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "9h 0m" },
+    { name: "DHFJGHD", id: 8, x: 800, y: 800, r: 80, color: "#67BB6D", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "4h 15m" },
+    { name: "JFGHDFG", id: 9, x: 900, y: 900, r: 50, color: "#3DC7C7", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "7h 30m" },
+    { name: "KJGDFHD", id: 10, x: 100, y: 900, r: 100, color: "#D4DC6C", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "11h 15m" },
+    { name: "DHFJGHD", id: 11, x: 200, y: 800, r: 50, color: "#9292C8", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "2h 45m" },
+    { name: "JFGHDFG", id: 12, x: 300, y: 700, r: 50, color: "#60DAAC", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "8h 0m" },
+    { name: "KJGDFHD", id: 13, x: 400, y: 600, r: 100, color: "#DB5657", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "5h 45m" },
+    { name: "DHFJGHD", id: 14, x: 500, y: 500, r: 50, color: "#67BB6D", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "12h 0m" },
+    { name: "JFGHDFG", id: 15, x: 600, y: 400, r: 80, color: "#3DC7C7", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "6h 15m" },
+    { name: "KJGDFHD", id: 16, x: 700, y: 300, r: 100, color: "#D4DC6C", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "9h 30m" },
+    { name: "DHFJGHD", id: 17, x: 800, y: 200, r: 50, color: "#9292C8", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "4h 45m" },
+    { name: "JFGHDFG", id: 18, x: 900, y: 100, r: 80, color: "#60DAAC", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "7h 0m" },
+    { name: "KJGDFHD", id: 19, x: 100, y: 100, r: 100, color: "#DB5657", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "10h 15m" },
+    { name: "DHFJGHD", id: 20, x: 200, y: 200, r: 50, color: "#67BB6D", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "3h 30m" },
+    { name: "JFGHDFG", id: 21, x: 300, y: 300, r: 50, color: "#3DC7C7", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "6h 45m" },
+    { name: "KJGDFHD", id: 22, x: 400, y: 400, r: 100, color: "#D4DC6C", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "11h 0m" },
+    { name: "DHFJGHD", id: 23, x: 500, y: 500, r: 50, color: "#9292C8", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "5h 30m" },
+    { name: "JFGHDFG", id: 24, x: 600, y: 600, r: 80, color: "#60DAAC", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "8h 45m" },
+    { name: "KJGDFHD", id: 25, x: 700, y: 700, r: 100, color: "#DB5657", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "4h 0m" },
+    { name: "DHFJGHD", id: 26, x: 800, y: 800, r: 50, color: "#67BB6D", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "9h 15m" },
+    { name: "JFGHDFG", id: 27, x: 900, y: 900, r: 80, color: "#3DC7C7", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "2h 30m" },
+    { name: "KJGDFHD", id: 28, x: 100, y: 900, r: 100, color: "#D4DC6C", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "7h 45m" },
+    { name: "DHFJGHD", id: 29, x: 200, y: 800, r: 50, color: "#9292C8", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "11h 0m" },
+    { name: "JFGHDFG", id: 30, x: 300, y: 700, r: 80, color: "#60DAAC", date: "2023-10-01", description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium", timeRequired: "5h 15m" }
+  ];
 
 var node = svg.selectAll(".node")
   .data(nodes)
@@ -52,15 +52,33 @@ node.append("circle")
   .attr("fill", d => d.color || "grey")
   .attr("stroke", "black")
 
-node.append("text")
-  .text(d => d.name)
-  .attr("text-anchor", "middle")
-  .attr("dy", "1em");
+let div = node.append("foreignObject")
+              .attr("width", d => d.r*2)
+              .attr("height", d => d.r*2)
+              .attr("x", d => -d.r)
+              .attr("y", d => -d.r)
 
-node.append("text")
-    .text(d => d.description)
-    .attr("text-anchor", "middle")
-    .attr("dy", "2.5em");
+let divBody = div.append("xhtml:body")
+                  .attr("class","bubble-body")
+                  .append("div")
+                  .attr("class","bubble-text-container")
+
+divBody.append("p")
+  .attr("class", "task-name")
+  .text(d => d.name)
+
+divBody.append("p")
+  .attr("class", "task-description task-hidden-info")
+  .text(d => d.description)
+
+divBody.append("p")
+  .attr("class", "task-date task-hidden-info")
+  .text(d => d.date)
+
+divBody.append("p")
+  .attr("class", "task-time task-hidden-info")
+  .text(d => d.timeRequired)
+
 
 let simulation = d3.forceSimulation(nodes)
     .force("x", d3.forceX(0).strength(0.05)) 
@@ -81,6 +99,7 @@ node.call(d3.drag()
   .on("start", dragstarted)
   .on("drag", dragged)
   .on("end", dragended));
+
 function dragstarted(event, d) {
   if (!event.active) simulation.alphaTarget(0.3).restart();
   d.fx = d.x;
@@ -103,10 +122,15 @@ function attachHoverListeners(elm){
   elm.on("mouseenter", (event, d) => {
     event.stopPropagation();
     hoverElemOrigRad = d.r;
-    if (d.r !== 100){
-      d.r = 100;
+    if (d.r !== 120){
+      d.r = 120;
       elm.raise();
       elm.select("circle").transition().duration(500).attr("r", d.r);
+
+      elm.select("foreignObject").attr("width", "240").attr("height", "240").attr("x", "-120").attr("y", "-120")
+      elm.select("foreignObject").selectAll(".task-hidden-info").style("display", "block");
+      // elm.select("foreignObject").select(".bubble-text-container").style("justify-content", "center");
+
       setTimeout(() => {
         simulation.force("collide", d3.forceCollide(d => d.r + 5).strength(0.5));
         simulation.alpha(0.1).restart();
@@ -116,9 +140,14 @@ function attachHoverListeners(elm){
 
   elm.on("mouseleave", (event, d) => {
     event.stopPropagation();
-    if (hoverElemOrigRad !== 100){
+    if (hoverElemOrigRad !== 120){
       d.r = hoverElemOrigRad;
       elm.select("circle").transition().duration(500).attr("r", d.r);
+
+      elm.select("foreignObject").attr("width", d.r*2).attr("height", d.r*2).attr("x", -d.r).attr("y", -d.r)
+      elm.select("foreignObject").selectAll(".task-hidden-info").style("display", "none");
+      // elm.select("foreignObject").select(".bubble-text-container").style("justify-content", "center");
+
       setTimeout(() => {
         simulation.force("collide", d3.forceCollide(d => d.r + 5).strength(0.8));
         simulation.alpha(0.1).restart();
